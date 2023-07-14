dd282b9900dea7847e95b14c6f0fce8414d80c81
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-14 19:08:09 +0300

    add questions and options

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,7 +1,21 @@
-import React from "react"
+import React, { useState } from "react"
+import { Questions } from "../Helpers/QuestionBank"
 
 function Quiz() {
-  return <div>Quiz</div>
+  const [currQuestion, setCurrQuestion] = useState(0)
+  const [optionChosen, setOptionChosen] = useState("")
+  return (
+    <div className="Quiz">
+      <h1>{Questions[currQuestion].prompt}</h1>
+      <div className="options">
+        <button>{Questions[currQuestion].optionA}</button>
+        <button>{Questions[currQuestion].optionB}</button>
+        <button>{Questions[currQuestion].optionC}</button>
+        <button>{Questions[currQuestion].optionD}</button>
+      </div>
+      <button>Next Question</button>
+    </div>
+  )
 }
 
 export default Quiz
