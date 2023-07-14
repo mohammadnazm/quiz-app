274a855ad51181be70d1114b27c112391934ba6b
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-14 20:06:40 +0300

    add score and next question

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,19 +1,38 @@
-import React, { useState } from "react"
+import React, { useState, useContext } from "react"
 import { Questions } from "../Helpers/QuestionBank"
+import { QuizContext } from "../Helpers/Contexts"
 
 function Quiz() {
+  const { score, setScore } = useContext(QuizContext)
   const [currQuestion, setCurrQuestion] = useState(0)
   const [optionChosen, setOptionChosen] = useState("")
+
+  const nextQuestion = () => {
+    if (Questions[currQuestion].answer === optionChosen) {
+      setScore(score + 1)
+    }
+    alert(score)
+    setCurrQuestion(currQuestion + 1)
+  }
+
   return (
     <div className="Quiz">
       <h1>{Questions[currQuestion].prompt}</h1>
       <div className="options">
-        <button>{Questions[currQuestion].optionA}</button>
-        <button>{Questions[currQuestion].optionB}</button>
-        <button>{Questions[currQuestion].optionC}</button>
-        <button>{Questions[currQuestion].optionD}</button>
+        <button onClick={() => setOptionChosen("A")}>
+          {Questions[currQuestion].optionA}
+        </button>
+        <button onClick={() => setOptionChosen("B")}>
+          {Questions[currQuestion].optionB}
+        </button>
+        <button onClick={() => setOptionChosen("C")}>
+          {Questions[currQuestion].optionC}
+        </button>
+        <button onClick={() => setOptionChosen("D")}>
+          {Questions[currQuestion].optionD}
+        </button>
       </div>
-      <button>Next Question</button>
+      <button onClick={nextQuestion}>Next Question</button>
     </div>
   )
 }
